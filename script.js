dfdf12ded011791399d9da37f343cc6750c9c4c7
Purple1984 <Nightbulb_@outlook.com>
2025-08-14 22:11:45 -0400

    tried to make page system work

--- script.js
+++ script.js
@@ -13,30 +13,14 @@ document.getElementById("NextButton").addEventListener("click", () => { NextPage
 
 async function init() {
     //set banner image
-    document.getElementById("bannerImage").src = "https://thenightbulb.neocities.org/PokeDictionary/PokeDictionary%20Logo.png";
+    document.getElementById("bannerImage").src = "https://raw.githubusercontent.com/TheNightbulb/PokeDictionaryV2/refs/heads/main/img/PokeDictionary%20Logo.png";
 
     //fetch list of pokemon from pokeAPI
     const pokelistTemp = await fetch("https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0");
     const PokeList = await pokelistTemp.json();
 
-    let container = document.getElementById("pokemon-grid");
     //make 10 tiles
-    for (let i = 0; i < 108; i++) { 
-
-        let tile = document.createElement("div");
-
-
-        tile.classList.add("pokemon-tile"); // for styling
-        //set pokemon name
-        tile.textContent = CapitalizeString(PokeList.results[i].name); 
-        container.appendChild(tile);
-
-        let img = document.createElement("img");
-        img.src = await GetPokemonSpriteLink(PokeList.results[i].url);
-        img.width = 100;
-        img.height = 100;
-        tile.appendChild(img);
-    }
+    UpdatePage();
     console.log("Init successfull");
 
 }
@@ -49,14 +33,41 @@ async function GetPokemonSpriteLink(PokemonLink) {
     const pokemon = await pokemonGet.json();
     return pokemon.sprites.front_default;
 }
+async function UpdatePage() {
+    let container = document.getElementById("pokemon-grid");
+    container.innerHTML = "";
+    for (let i = 108 * (page - 1); i < 108 * page; i++) {
 
+        let tile = document.createElement("div");
+
+
+        tile.classList.add("pokemon-tile"); // for styling
+        //set pokemon name
+        tile.textContent = CapitalizeString(PokeList.results[i].name);
+        container.appendChild(tile);
+
+        let img = document.createElement("img");
+        img.src = await GetPokemonSpriteLink(PokeList.results[i].url);
+        img.width = 100;
+        img.height = 100;
+        tile.appendChild(img);
+    }
+}
 
 
 function NextPageButton() {
-    page++;
+    if (PokeList.results[page * 108]) {
+        page++;
+        UpdatePage();
+    }
+
     console.log("Went to next page: " + page);
 }
 function LastPageButton() {
-    page--;
+    if (page != 1) {
+        page--;
+        UpdatePage();
+    }
+
     console.log("Went to previous page: " + page);
 }
